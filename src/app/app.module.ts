088bda60c83bd14a22494bc10ce35bd797f5e6dc
Vytenis <43178578+vytenis999@users.noreply.github.com>
2022-08-04 11:51:03 +0300

    First Init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,17 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ButtonModule } from 'primeng/button';
+import { LoginComponent } from './components/login/login.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, LoginComponent, PageNotFoundComponent],
+  imports: [BrowserModule, AppRoutingModule, ButtonModule, HttpClientModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
